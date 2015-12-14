66ea4cdd8e151d4212ddeb19073b2527d660b163
Mauro de Lyon <maurodelyon@gmail.com>
2015-12-14 15:53:28 +0100

    V1

---
+++ Channel9/UWP23/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWP23
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            myFrame.Navigate(typeof(Financial));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            splitView.IsPaneOpen = !splitView.IsPaneOpen;
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (number1.IsSelected)
+            {
+                PageName.Text = "Financial";
+                myFrame.Navigate(typeof(Financial));
+            }
+            else if (number2.IsSelected)
+            {
+                PageName.Text = "Food";
+                myFrame.Navigate(typeof(Food));
+            }
+        }
+    }
+}
